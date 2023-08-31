4ce86f1b4a2dbc06b0dc21ac7d2db81517210393
Dheeraj <avarjana348@gmail.com>
2023-09-01 01:17:51 +0530

    adding react and server

---
+++ React/WebsiteDesign/src/App.jsx
@@ -0,0 +1,21 @@
+
+import Contact from './Contact'
+import Home from './Home'
+import Services from './Services'
+import About from './About'
+const App = ()=> {
+
+  return(
+  <>
+   <Home/>
+   <Contact />
+   <Services/>
+   <About/>
+   </>
+  )
+
+
+
+
+}
+export default App;
